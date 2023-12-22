23d44abc5efe0838dfee7cfe319d3f73c1aa6d69
hari <hari.harik@gmail.com>
2023-12-22 00:54:31 +0000

    sanitized adminlogin

--- classess/Adminlogin.php
+++ classess/Adminlogin.php
@@ -1,73 +1,61 @@
-
 <?php
 $filepath = realpath(dirname(__FILE__));
-include_once ($filepath.'/../lib/Session.php');
+include_once($filepath.'/../lib/Session.php');
 Session::checkLogin();
 
-include_once ($filepath.'/../lib/Database.php');
-include_once ($filepath.'/../helpers/Formate.php');
+include_once($filepath.'/../lib/Database.php');
+include_once($filepath.'/../helpers/Formate.php');
 
 ?>
 
-
-
 <?php
-
 
 class Adminlogin
 {
-	
-private $db;
-private $fm;
 
-	public function __construct()
-	{
-		
-		$this->db = new Database();
-		$this->fm = new Format();
-	}
+    private $db;
+    private $fm;
 
-	public function adminlogin($adminUser,$adminPassword) 
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->fm = new Format();
+    }
 
-	
+    public function adminlogin($adminUser, $adminPassword)
+    {
 
+        if (empty($adminUser) || empty($adminPassword)) {
 
-	{
+            $loginmsg = "Username or Password must not be empty !";
+            return $loginmsg;
+        } else {
 
-if (empty($adminUser) ||empty($adminPassword) ) {
-	
-	$loginmsg = "Username or Password must not be empty !";
-	return $loginmsg;
-		} else{
+            $adminUser = mysqli_escape_string($this->db->link, $adminUser);
+            $adminPassword = mysqli_escape_string($this->db->link, $adminPassword);
 
-
-			$query = "SELECT * FROM tbl_admin WHERE adminUser = '$adminUser'
+            $query = "SELECT * FROM tbl_admin WHERE adminUser = '$adminUser'
 			AND adminPassword = '$adminPassword'";
-			$result = $this->db->select($query);
+            $result = $this->db->select($query);
 
-			if ($result != false) {
-				$value = $result->fetch_assoc();
+            if ($result != false) {
+                $value = $result->fetch_assoc();
 
-				Session::set("adminlogin",true);
-				Session::set("adminId",$value['adminId']);
-				Session::set("adminUser",$value['adminUser']);
-				Session::set("adminName",$value['adminName']);
-				
-				$log  = "Admin Logged in: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-				"User:".$value['adminUser'].PHP_EOL.
-				"-------------------------".PHP_EOL;
-				file_put_contents('../log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
-				header("Location:dashboard.php");
-			} else{
-				$loginmsg = "Username or Password not match !";
-				return $loginmsg;
-			}
+                Session::set("adminlogin", true);
+                Session::set("adminId", $value['adminId']);
+                Session::set("adminUser", $value['adminUser']);
+                Session::set("adminName", $value['adminName']);
 
-
-		}
-
-	}
+                $log  = "Admin Logged in: " . $_SERVER['REMOTE_ADDR'] . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
+                    "User:" . $value['adminUser'] . PHP_EOL .
+                    "-------------------------" . PHP_EOL;
+                file_put_contents('../log_' . date("j.n.Y") . '.log', $log, FILE_APPEND);
+                header("Location:dashboard.php");
+            } else {
+                $loginmsg = "Username or Password not match !";
+                return $loginmsg;
+            }
+        }
+    }
 }
-
-
 ?>
